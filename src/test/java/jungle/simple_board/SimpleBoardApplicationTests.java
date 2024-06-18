43023dba8103b26fdcf16bdd03fdf10f75cbc556
ivk <leorivk@gmail.com>
2024-06-18 19:15:21 +0900

    Initiailize

---
+++ src/test/java/jungle/simple_board/SimpleBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package jungle.simple_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
